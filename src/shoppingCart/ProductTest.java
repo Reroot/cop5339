13c8df1253251baca3dd85c8e67ac0343a10684e
Seth J. Moore <seth@sethjmoore.com>
2013-11-10 11:01:28 -0500

    Generated JUnit test case for Product.

---
+++ src/shoppingCart/ProductTest.java
@@ -0,0 +1,160 @@
+/**
+ * 
+ */
+package shoppingCart;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Seth
+ *
+ */
+public class ProductTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#Product(int, java.lang.String, java.lang.String, java.math.BigDecimal, java.math.BigDecimal, int)}.
+	 */
+	@Test
+	public void testProduct() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#update(int, java.lang.String, java.lang.String, java.math.BigDecimal, java.math.BigDecimal, int)}.
+	 */
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#increment()}.
+	 */
+	@Test
+	public void testIncrement() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#decrement()}.
+	 */
+	@Test
+	public void testDecrement() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#getID()}.
+	 */
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#getDescription()}.
+	 */
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#getSellPrice()}.
+	 */
+	@Test
+	public void testGetSellPrice() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#getInvoicePrice()}.
+	 */
+	@Test
+	public void testGetInvoicePrice() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#getQuantity()}.
+	 */
+	@Test
+	public void testGetQuantity() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#addListener(javax.swing.event.ChangeListener)}.
+	 */
+	@Test
+	public void testAddListener() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link shoppingCart.Product#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
