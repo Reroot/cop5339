08d976f53a86b2737d4453db5655558b85dab3c8
Seth J. Moore <seth@sethjmoore.com>
2013-11-17 15:53:27 -0500

    Got UI started displaying with a tester.

--- src/shoppingCart/UI.java
+++ src/shoppingCart/UI.java
@@ -1,18 +1,46 @@
 package shoppingCart;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
 /** A class that manages interaction with user, receives input and display screens.
  *  @author Seth Moore and Newman Souza
  */ 
-public class UI {
+public class UI extends JFrame{
 	
     /** Constructs a UI object.
      *  @precondition none
      *  @postcondition object created
      */
     public UI() {
-
-    	// code
+    	super("Shopping Cart");
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    	setPreferredSize(new Dimension(400, 400));
+    	setLayout(new BorderLayout());
     	
+    	JPanel screenNamePanel = new JPanel();
+    	screenNamePanel.setPreferredSize(new Dimension(400, 50));
+    	screenNamePanel.setBorder(new EtchedBorder());
+    	add(screenNamePanel, BorderLayout.PAGE_START);
+    	
+    	JPanel browsePanel = new JPanel();
+    	browsePanel.setPreferredSize(new Dimension(300, 350));
+    	browsePanel.setBorder(new EtchedBorder());
+    	add(browsePanel, BorderLayout.CENTER);
+    	
+    	JPanel sidePanel = new JPanel();
+    	sidePanel.setPreferredSize(new Dimension(100, 350));
+    	sidePanel.setBorder(new EtchedBorder());
+    	add(sidePanel, BorderLayout.LINE_END);
+    	
+    	pack();
+    	setVisible(true);
+    	
+    	displayLoginScreen();
     }
 
     /** .
@@ -69,9 +97,9 @@ public class UI {
      *  @precondition 
      *  @postcondition 
      */
-    public void displaySellerScreen(ProductList inventory, String data) {
+    public void displaySellerScreen(Inventory inventory) {
 
-    	// code
+    	this.inventory = inventory;
     	
     }
 
@@ -93,9 +121,9 @@ public class UI {
      *  @precondition 
      *  @postcondition 
      */
-    public void displayCustomerScreen(ProductList available, ProductList cart) {
+    public void displayCustomerScreen(Inventory inventory) {
 
-    	// code
+    	this.inventory = inventory;
     	
     }
 
@@ -111,52 +139,7 @@ public class UI {
     	
     }
 
-    /** .
-     *  @param 
-     *  @return ?
-     *  @precondition 
-     *  @postcondition 
-     */
-    public void displayFailMessage() {
-
-    	// code
-    	
-    }
-
-    /** .
-     *  @param 
-     *  @return ?
-     *  @precondition 
-     *  @postcondition 
-     */
-    public void displayDetail(Product product) {
-
-    	// code
-    	
-    }
-
-    /** .
-     *  @param 
-     *  @return ?
-     *  @precondition 
-     *  @postcondition 
-     */
-    public void displaySaleConfirmation() {
-
-    	// code
-    	
-    }
-
-    /** .
-     *  @param 
-     *  @return ?
-     *  @precondition 
-     *  @postcondition 
-     */
-    public void requestDeleteConfirmation() {
-
-    	// code
-    	
-    }
+    private Inventory inventory;
+    private Cart cart;
 
 }
