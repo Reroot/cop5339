fd958ade4a847b6d8f9d6c8fa336d66caaf652b8
Seth J. Moore <seth@sethjmoore.com>
2013-11-17 23:27:12 -0500

    Updated UI.

--- src/shoppingCart/UI.java
+++ src/shoppingCart/UI.java
@@ -6,9 +6,13 @@ import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 
 /** A class that manages interaction with user, receives input and display screens.
@@ -25,22 +29,14 @@ public class UI extends JFrame{
      */
     public UI() {
     	super("Shopping Cart");
+    	inventory = Inventory.getInstance();
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //    	setPreferredSize(new Dimension(400, 400));
     	screenCards = new JPanel(new CardLayout());
     	
     	//setLayout(new BorderLayout());
     	
-    	JPanel loginScreen = new JPanel();
-    	JButton loginButton = new JButton("Login");
-    	loginButton.addMouseListener(new
-    			MouseAdapter(){
-    				public void mouseClicked(MouseEvent e) {
-    					((CardLayout)(screenCards.getLayout())).show(screenCards, USINGPANEL);
-    				};
-    			}
-    		);
-    	loginScreen.add(loginButton);
+    	JPanel loginScreen = createLoginScreen();
     	screenCards.add(loginScreen, LOGINPANEL);
     	
     	JPanel usingScreen = new JPanel();
@@ -71,6 +67,8 @@ public class UI extends JFrame{
     	
     	displayLoginScreen();
     }
+
+	
 
     /** .
      *  @param 
@@ -139,9 +137,9 @@ public class UI extends JFrame{
      *  @postcondition 
      */
     public void displayCustomerScreen() {
-
-    	// code
-    	
+    	PrunningIterator PIter = new PrunningIterator(inventory.iterator());
+//    	customerBrowsePanel.populate(PIter);
+    	((CardLayout)(screenCards.getLayout())).show(screenCards, USINGPANEL);
     }
 
     /** .
@@ -167,9 +165,50 @@ public class UI extends JFrame{
     	// code
     	
     }
+    
+    private JPanel createLoginScreen() {
+		JPanel loginScreen = new JPanel();
+		loginScreen.setLayout(new BorderLayout());
+		JPanel topPanel = new JPanel();
+		topPanel.add(new JLabel("Login Screen"));
+		topPanel.setBorder(new EtchedBorder());
+		loginScreen.add(topPanel, BorderLayout.PAGE_START);
+		
+		JPanel centerPanel = new JPanel();
+		loginScreen.add(centerPanel, BorderLayout.CENTER);
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+		
+		final JTextField usernameField = new JTextField();
+		final JTextField passwordField = new JTextField();
+    	JButton loginButton = new JButton("Login");
+    	loginButton.addMouseListener(new
+    			MouseAdapter(){
+    				public void mouseClicked(MouseEvent e) {
+    					// cartSystem.login(usernameField.getText(), passwordField.getText())
+    					displayCustomerScreen();
+//    					if (usernameField.getText().equals("username") && passwordField.getText().equals("password")){
+//    						((CardLayout)(screenCards.getLayout())).show(screenCards, USINGPANEL);
+//    					}
+//    					else {
+//    						JOptionPane.showMessageDialog(screenCards, "Try again!");
+//    						usernameField.setText("");
+//    						passwordField.setText("");
+//    						
+//    					}
+    					
+    				};
+    			}
+    		);
+    	centerPanel.add(usernameField);
+    	centerPanel.add(passwordField);
+    	centerPanel.add(loginButton);
+		return loginScreen;
+	}
 
+    private CartSystem cartSystem;
     private Inventory inventory;
     private Cart cart;
+//    private AbstractBrowsePanel customerBrowsePanel;
     JPanel screenCards;
 
 }
